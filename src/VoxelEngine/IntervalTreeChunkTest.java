413d43f435b417483893281c974899a1a6dcc1da
EliArtist <elias-kuenstler@web.de>
2016-04-11 22:52:33 +0200

    Added TestClass, not yet usable

---
+++ src/VoxelEngine/IntervalTreeChunkTest.java
@@ -0,0 +1,20 @@
+package VoxelEngine;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by elias on 4/11/16.
+ */
+
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class IntervalTreeChunkTest {
+
+    @Test
+    public void testAdd() {
+        String str = "Hallo Welt";
+        assertEquals("Hallo Welt", str);
+    }
+
+}
