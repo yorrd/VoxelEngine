e19cdedef08881f93b4e05a564ba99a548f3b292
EliArtist <elias-kuenstler@web.de>
2016-04-19 16:47:25 +0200

    Added TestClass for IntervalTree

--- src/VoxelEngine/IntervalTreeChunkTest.java
+++ src/VoxelEngine/IntervalTreeChunkTest.java
@@ -2,19 +2,63 @@ package VoxelEngine;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by elias on 4/11/16.
- */
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert.*;
 
 public class IntervalTreeChunkTest {
 
-    @Test
-    public void testAdd() {
-        String str = "Hallo Welt";
-        assertEquals("Hallo Welt", str);
+    World world;
+    TerrainGenerator generator;
+    Chunk[] neighbors;
+
+    IntervalTreeChunk test;
+    Block[] shouldBe = new Block[16];
+
+    @Before
+    public void beforeTest() {
+
+        world = new World();
+        generator = new ChaosTerrainGenerator(0, 0, 0);
+        neighbors = new Chunk[0];
+
+        test = new IntervalTreeChunk(world, generator, neighbors);
+
+        for(int i = 0; i < 16; i++) {
+            Block tmp = new Block(Block.BlockType.getRandom());
+
+            shouldBe[i] = tmp;
+
+            test.set((short)i, (short)0, (short)0, tmp);
+        }
+
+
     }
 
+    @Test
+    public void testGetBlock() {
+
+        for (int i = 0; i < 16; i++) {
+            Block reference = shouldBe[i];
+
+            Block toTest = test.get((short)i, (short)0, (short)0);
+
+            assertEquals(toTest.toString(), reference.toString());
+        }
+    }
+
+    @Test
+    public void testGetInterval() {
+        //TODO Fehler eventuell in getInterval-Methode bei Array Initialisierung des Rückgabe-Arrays
+        Block[][][] toTest = test.getInterval((short)0, (short)16, (short)0, (short)1, (short)0, (short)1);
+
+        for (int i = 0; i < 16; i++) {
+            Block reference = shouldBe[i];
+
+            Block toTestBlock = toTest[0][0][i];
+
+            assertEquals(toTestBlock.toString(), reference.toString());
+        }
+
+    }
 }
